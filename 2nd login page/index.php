5e36237a663771be8d27cd06bac4b66c48420fb1
robotic-neutraliser <105120661+robotic-neutraliser@users.noreply.github.com>
2023-09-13 02:07:02 +0530

    Add files via upload

---
+++ 2nd login page/index.php
@@ -0,0 +1,71 @@
+<?php
+require 'config.php';
+if(isset($_POST["register"])){
+  $name = $_POST["name"];
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+  $phone = $_POST["phone"];
+
+  $user = mysqli_query($conn, "SELECT * FROM tb_users WHERE username = '$username' OR email = '$email'");
+  if(mysqli_num_rows($user) > 0){
+    echo
+    "
+    <script> alert('Username/Email Has Already Taken'); </script>
+    ";
+  }
+  else{
+    $query = "INSERT INTO tb_users VALUES('', '$name', '$username', '$email', '$password', '$phone')";
+    mysqli_query($conn, $query);
+    echo
+    "
+    <script> alert('Registration Successful'); </script>
+    ";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Register</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <div class="title">
+        <h2>REGISTER</h2>
+      </div>
+      <div class="half">
+        <div class="item">
+          <label for="">Name</label>
+          <input type="text" name="name" required value="">
+        </div>
+        <div class="item">
+          <label for="">Username</label>
+          <input type="text" name="username" required value="">
+        </div>
+      </div>
+      <div class="half">
+        <div class="item">
+          <label for="">Email</label>
+          <input type="text" name="email" required value="">
+        </div>
+        <div class="item">
+          <label for="">Password</label>
+          <input type="password" name="password" required value="">
+        </div>
+      </div>
+      <div class="full">
+        <div class="item">
+          <label for="">Phone</label>
+          <input type="text" name="phone" required value="">
+        </div>
+      </div>
+      <div class="action">
+        <input type="submit" name = "register" value = "REGISTER">
+      </div>
+    </form>
+  </body>
+</html>
